dcab4189ea1b284ed38ba8e3a1d81bbf7d14a225
MaxDorob <maxdor81@gmail.com>
2024-09-02 03:13:54 +0600

    Source init

---
+++ Source/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Situational_Animal_Graphics
+{
+    public class StartUp
+    {
+    }
+}
