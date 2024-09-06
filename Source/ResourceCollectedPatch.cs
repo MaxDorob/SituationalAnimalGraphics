b6cd2eeaaaeeffd2f1a35bede1751e06ad822e32
MaxDorob <maxdor81@gmail.com>
2024-09-07 01:32:02 +0600

    Another effecter spawn

--- Source/ResourceCollectedPatch.cs
+++ Source/ResourceCollectedPatch.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using Verse.Noise;
 
 namespace SituationalAnimalGraphics
 {
@@ -13,7 +14,13 @@ namespace SituationalAnimalGraphics
     {
         static void Postfix(CompHasGatherableBodyResource __instance)
         {
-            (__instance.parent as Pawn).Drawer.renderer.renderTree.SetDirty();
+            var pawn = (__instance.parent as Pawn);
+            var map = pawn.Map;
+            pawn.Drawer.renderer.renderTree.SetDirty();
+            if (pawn.ageTracker.CurKindLifeStage is SeasonalGraphics seasonalGraphics && seasonalGraphics.TryGetGraphic(Utils.GetSeason(map, Find.TickManager.TicksAbs), pawn.Faction != null, pawn.gender == Gender.Female, pawn.TryGetComp<CompHasGatherableBodyResource>()?.Fullness ?? 1f, out _, out var effecter))
+            {
+                effecter?.Spawn(pawn.Position, map);
+            }
         }
     }
 }
--- Source/TamedPatch.cs
+++ Source/TamedPatch.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using Verse.Noise;
 
 namespace SituationalAnimalGraphics
 {
@@ -15,6 +17,11 @@ namespace SituationalAnimalGraphics
             if (__instance.RaceProps.Animal)
             {
                 __instance.Drawer.renderer.renderTree.SetDirty();
+                var map = __instance.Map;
+                if (__instance.ageTracker.CurKindLifeStage is SeasonalGraphics seasonalGraphics && seasonalGraphics.TryGetGraphic(Utils.GetSeason(map, Find.TickManager.TicksAbs), __instance.Faction != null, __instance.gender == Gender.Female, __instance.TryGetComp<CompHasGatherableBodyResource>()?.Fullness ?? 1f, out _, out var effecter))
+                {
+                    effecter?.Spawn(__instance.Position, map);
+                }
             }
         }
     }
