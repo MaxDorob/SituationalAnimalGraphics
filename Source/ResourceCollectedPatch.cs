0e0ff7d88d2e6a70bf0cb4b40ed7a0daf6d8f068
MaxDorob <maxdor81@gmail.com>
2024-09-06 23:52:55 +0600

    Patches to update graphics on changed state

---
+++ Source/ResourceCollectedPatch.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace SituationalAnimalGraphics
+{
+    [HarmonyLib.HarmonyPatch(typeof(CompHasGatherableBodyResource), nameof(CompHasGatherableBodyResource.Gathered))]
+    internal static class ResourceCollectedPatch
+    {
+        static void Postfix(CompHasGatherableBodyResource __instance)
+        {
+            (__instance.parent as Pawn).Drawer.renderer.renderTree.SetDirty();
+        }
+    }
+}
---
+++ Source/TamedPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace SituationalAnimalGraphics
+{
+    [HarmonyLib.HarmonyPatch(typeof(Pawn), nameof(Pawn.SetFaction))]
+    internal static class TamedPatch
+    {
+        static void Postfix(Pawn __instance)
+        {
+            if (__instance.RaceProps.Animal)
+            {
+                __instance.Drawer.renderer.renderTree.SetDirty();
+            }
+        }
+    }
+}
