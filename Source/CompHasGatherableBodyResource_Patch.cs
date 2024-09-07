e6c0033f54642907b02f096377e230d496e393a8
MaxDorob <maxdor81@gmail.com>
2024-09-08 02:44:59 +0600

    Random fullness on spawn

---
+++ Source/CompHasGatherableBodyResource_Patch.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace SituationalAnimalGraphics
+{
+    [HarmonyLib.HarmonyPatch(typeof(ThingComp), nameof(ThingComp.PostSpawnSetup))]
+    internal static class CompHasGatherableBodyResource_Patch
+    {
+        public static void Postfix(ThingComp __instance, bool respawningAfterLoad)
+        {
+            if (!respawningAfterLoad && __instance is CompHasGatherableBodyResource gatherableBodyResource)
+            {
+                gatherableBodyResource.fullness = Rand.Range(0.85f, 1f);
+            }
+        }
+    }
+}
